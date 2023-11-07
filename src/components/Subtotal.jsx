6c2aacda8780dd4930ec7cda5e20ca4513e15bd4
morteza20deris <mtz20dri@gmail.com>
2023-11-07 19:45:30 +0330

    little bug fix

--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -1,15 +1,13 @@
 import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import CurrencyFormat from 'react-currency-format'
+import { useNavigate } from "react-router-dom"
 import { auth, database } from '../Services/firebase'
 import getBasketTotal from '../Services/getBasketTotal'
 import globalStore from '../Services/globalStore'
-import { useNavigate } from "react-router-dom";
 import "./Subtotal.css"
-import { CircularProgress } from '@mui/material'
 function Subtotal() {
   const kartItems = globalStore(store => store.kartItems)
-  const [showLoader, setShowLoader] = useState(false)
   const navigate = useNavigate()
   
   useEffect(() => {
@@ -28,9 +26,9 @@ function Subtotal() {
       document.getElementById("checkout").style.filter = ""
       navigate("/orders")
     }, 5000);
-    // const test1 = doc(database, "Users",auth.currentUser.email);
-    // const test2 = doc(test1, "Orders",Math.random()*100000+"")
-    // setDoc(test2, { cart: kartItems, date: serverTimestamp() })
+    const test1 = doc(database, "Users",auth.currentUser.email);
+    const test2 = doc(test1, "Orders",Math.random()*100000+"")
+    setDoc(test2, { cart: kartItems, date: serverTimestamp() })
   }
   return (
     <div className='subtotal'>
