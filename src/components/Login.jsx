40ffc32d66d1497736873269857c0b4e2a5e5106
morteza20deris <mtz20dri@gmail.com>
2023-11-17 15:11:48 +0330

    +show Please wait on registe or login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,30 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import amazonLogo from "../Assets/amazon-logo-black.png";
 import { auth } from '../Services/firebase';
 import "./Login.css";
 function Login() {
     const navigate = useNavigate()
+    const loginButtonRef = useRef(null)
+    const registerButtonRef = useRef(null)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const formSubmitHandler = (e) => {
+        loginButtonRef.current.textContent = "Please Wait..."
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
             .then(()=>navigate('/'))
-            .catch(err=>alert(err.code.replace("-"," ").replace("auth/","")))
+            .catch(err => alert(err.code.replace("-", " ").replace("auth/", "")))
+            .finally(()=>loginButtonRef.current.textContent = "Sign In")
     }
 
     const registerButtonHandler = () => {
+        registerButtonRef.current.textContent = "Please Wait..."
         createUserWithEmailAndPassword(auth, email, password)
             .then(()=>navigate('/'))
-            .catch(err=>alert(err.code.replaceAll("-"," ").replace("auth/","")))
+            .catch(err => alert(err.code.replaceAll("-", " ").replace("auth/", "")))
+            .finally(()=>registerButtonRef.current.textContent = "Create Account")
     }
   return (
       <div className='login'>
@@ -33,9 +39,9 @@ function Login() {
                   <input id='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} type="password" />
                   {password === '' && <p>Password is Required</p>}
                   {password.length <8 && <p>Password must be at least 8 characters long</p>}
-                  <button type='submit' className='login__signInButton'>Sign In</button>
+                  <button disabled={loginButtonRef.current.textContent === "Please Wait..."} ref={loginButtonRef} type='submit' className='login__signInButton'>Sign In</button>
               </form>
-              <button onClick={registerButtonHandler} className='login__registerButton'>Create Account</button>
+              <button disabled={registerButtonRef.current.textContent === "Please Wait..."} ref={registerButtonRef}  onClick={registerButtonHandler} className='login__registerButton'>Create Account</button>
           </div>
     </div>
   )
